0f8a516527c3fba2dd40f18df7804b3fcee2e015
Shakeeb ur Rehman <shakeeburrehman78@gmail.com>
2024-12-24 17:16:49 +0500

    updated settings

--- PharmX/settings.py
+++ PharmX/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q+h@rql&j0$k40!f!cu(@pi@0_!p7$q+0&m^@4dfrfrs#f7n12'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 

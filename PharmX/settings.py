7ac182b9e1e76bbb7bc2b5b83a0fae66c50ce1b9
Shakeeb ur Rehman <shakeeburrehman78@gmail.com>
2024-12-26 10:14:47 +0500

    sqlite3 DB added

--- PharmX/settings.py
+++ PharmX/settings.py
@@ -67,15 +67,10 @@ WSGI_APPLICATION = 'PharmX.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sql12754196',  
-        'USER': 'sql12754196',      
-        'PASSWORD': 'ghvWMk6MgM',   
-        'HOST': 'sql12.freesqldatabase.com',
-        'PORT' : '3306',         
+        'ENGINE': 'django.db.backends.sqlite3', 
+        'NAME': BASE_DIR / 'db.sqlite3',      
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
